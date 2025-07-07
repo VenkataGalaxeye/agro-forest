b845e84990bbae9ff394695a14aef9c7c60bf22b
VenkataGalaxeye <venkata.ramana@galaxeye.space>
2025-07-07 21:14:15 +0530

    third commit by venkat

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export const areas = [
   {
-    name: "Agroforestry Map",
+    name: "Agroforestry Map_1",
     img: "kml_2.gif",
     stats: {
       totalTrees: 170,
@@ -8,35 +8,59 @@ export const areas = [
     }
   },
   {
-    name: "Agroforestry Fields",
-    img: "kml_2.gif",
+    name: "Map1(2022) Tree Count",
+    img: "map1_2022_trees.png",
     stats: {
       totalTrees: 303,
       totalArea: 1289.25
     }
   },
   {
-    name: "Field 1",
-    img: "kml_2.gif",
+    name: "Map1(2025) Tree Count",
+    img: "map1_2025_trees.png",
     changes: {
       pondSplit: "29 --> 62",
       pondMerge: "24 --> 11"
     }
   },
   {
-    name: "Time Series",
+    name: "Map1 Heatmap",
     img: "kml_2.gif",
     changes: {
       pondSplit: "29 --> 62",
       pondMerge: "24 --> 11"
     }
   },
-  {
-    name: "Health",
-    img: "kml_2.gif",
+    {
+    name: "Agroforestry Map_2",
+    img: "kml_1.gif",
     stats: {
-      totalTrees: 234,
-      totalArea: 871.87
+      totalTrees: 170,
+      totalArea: 645.84
     }
-  }
+  },
+  {
+    name: "Map2(2022) Tree Count",
+    img: "map2_2022_trees.png",
+    stats: {
+      totalTrees: 303,
+      totalArea: 1289.25
+    }
+  },
+  {
+    name: "Map2(2025) Tree Count",
+    img: "map2_2025_trees.png",
+    changes: {
+      pondSplit: "29 --> 62",
+      pondMerge: "24 --> 11"
+    }
+  },
+  {
+    name: "Map2 Heatmap",
+    img: "kml_2.gif",
+    changes: {
+      pondSplit: "29 --> 62",
+      pondMerge: "24 --> 11"
+    }
+  },
 ];
